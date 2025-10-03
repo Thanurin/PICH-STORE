4bd5fff799260d1ca30a0339e9cc0eaa26e700db
Nu <191132814+Thanurin@users.noreply.github.com>
2025-10-03 03:09:22 -0400

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("click", function() {
+    window.location.href = "ទំនិញ.html";
+});
+document.querySelectorAll("button")[1].addEventListener("click", function() {
+    window.location.href = "ទំនាក់ទំនង.html";
+});
---
+++ js3.js
@@ -0,0 +1,9 @@
+document.querySelector("button").addEventListener("click", function(){
+    window.location.href= "Home.html";
+});
+document.querySelectorAll("button")[1].addEventListener("click", function() {
+    window.location.href= "https://www.facebook.com/leableangsrey";
+});
+document.querySelectorAll("button")[2].addEventListener("click", function() {
+    window.location.href= "https://t.me/pichstore11";
+});
